07649db4d7834a4554a565068bb42a5781b008ce
fl <1473357859@qq.com>
2017-09-21 10:20:44 +0800

    微服务的服务追踪

---
+++ service-miya/src/main/java/com/springboot/example/ServiceMiyaApplication.java
@@ -0,0 +1,45 @@
+package com.springboot.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+public class ServiceMiyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceMiyaApplication.class, args);
+	}
+	private static final Logger log = LoggerFactory.getLogger(ServiceMiyaApplication.class);
+
+
+
+	@RequestMapping("/hi")
+	public String home(){
+		//LOG.log(Level.INFO, "hi is being called");
+		log.info("hi is being called");
+		return "hi i'm miya!";
+	}
+
+	@RequestMapping("/miya")
+	public String info(){
+		//LOG.log(Level.INFO, "info is being called");
+		log.info("info is being called");
+		return restTemplate.getForObject("http://localhost:8988/info",String.class);
+	}
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
+}
